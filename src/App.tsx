0a6996b005388fa56e8b6e0df70ccc6e0344db04
ich1ro <pavel.dubenchuk.02@gmail.com>
2023-05-10 19:25:27 +0300

    add router for main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import './App.css';
 
 import Main from './components/Main';
+import { Route, Routes } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="wrapper">
-      <Main />
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<div className="wrapper">
+				<Routes>
+					<Route path="/" element={<Main />} />
+					<Route path="/detail"/>
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
