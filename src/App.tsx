639803a8954112f882cbe7790544701ea6550535
ich1ro <pavel.dubenchuk.02@gmail.com>
2023-05-14 00:14:21 +0300

    remove browser router from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,22 @@ import './App.css';
 
 import Main from './components/Main';
 import { Route, Routes } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
 import CloudOutlinedIcon from '@mui/icons-material/CloudOutlined';
 
 function App() {
 	return (
-		<BrowserRouter>
-		<div className='header'>
-			<h3 className='title'>Weather app</h3>
-			<CloudOutlinedIcon />
-		</div>
+		<>
+			<div className="header">
+				<h3 className="title">Weather app</h3>
+				<CloudOutlinedIcon />
+			</div>
 			<div className="wrapper">
 				<Routes>
 					<Route path="/" element={<Main />} />
-					<Route path="/detail"/>
+					<Route path="/detail" />
 				</Routes>
 			</div>
-		</BrowserRouter>
+		</>
 	);
 }
 
