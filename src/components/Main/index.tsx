6d3cd243db9acc64353b20f8432ddb8467afbb22
ich1ro <pavel.dubenchuk.02@gmail.com>
2023-05-17 01:11:31 +0300

    create serach, write logic for serach and buttons

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -3,28 +3,153 @@ import './Main.css';
 
 import WeatherCard from '../WeatherCard';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import { useCustomDispatch, useCustomSelector } from '../../hooks/store';
+import CloseIcon from '@mui/icons-material/Close';
+import Сities from '../../cities.json';
+import { useCustomDispatch } from '../../hooks/store';
 import { fetchCurrentWeather } from '../../store/thunks/fetchCurrentWeather';
+import { City, Weather } from '../../store/@types/types';
+import { deleteCurrentWeather } from '../../store/slices/currentWeatherSlice';
+import Empty from '../Empty';
 
-const Main = () => {
+interface Props {
+	weather: Weather[];
+}
+
+const Main = ({ weather }: Props) => {
+	const [filteredData, setFilteredData] = React.useState<City[]>([]);
+	const [wordEntered, setWordEntered] = React.useState('');
+	const [selectedCities, setSelectedCities] = React.useState<string[]>([]);
+
 	const dispatch = useCustomDispatch();
 
-	const { weather } = useCustomSelector(
-		state => state.currentWeatherSliceReducer
-	);
-
 	useEffect(() => {
-		dispatch(fetchCurrentWeather('Kyiv'));
+		const savedCities = localStorage.getItem('selectedCities');
+		if (savedCities) {
+			setSelectedCities(JSON.parse(savedCities));
+		}
 	}, []);
+
+	const handleFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const searchCity = event.target.value;
+		setWordEntered(searchCity);
+		const newFilter = (Сities as City[]).filter((value: City) => {
+			return value.name.toLowerCase().includes(searchCity.toLowerCase());
+		});
+		if (searchCity === '') {
+			setFilteredData([]);
+		} else {
+			setFilteredData(newFilter);
+		}
+	};
+
+	const handleCityClick = async (city: string) => {
+		if (!selectedCities.includes(city)) {
+			try {
+				await dispatch(fetchCurrentWeather(city));
+				setSelectedCities(prevCities => [...prevCities, city]);
+
+				const citiesInLocalStorage =
+					localStorage.getItem('selectedCities');
+				let parsedCities = citiesInLocalStorage
+					? JSON.parse(citiesInLocalStorage)
+					: [];
+				parsedCities.push(city);
+
+				localStorage.setItem(
+					'selectedCities',
+					JSON.stringify(parsedCities)
+				);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		setWordEntered('');
+		setFilteredData([]);
+	};
+
+	const clearInput = () => {
+		setFilteredData([]);
+		setWordEntered('');
+	};
+
+	const onDelete = (city: string, name: string) => {
+		dispatch(deleteCurrentWeather(name));
+		setSelectedCities(prevCities =>
+			prevCities.filter(prevCity => prevCity !== city)
+		);
+
+		const citiesInLocalStorage = localStorage.getItem('selectedCities');
+		if (citiesInLocalStorage) {
+			const parsedCities = JSON.parse(citiesInLocalStorage);
+			const updatedCities = parsedCities.filter(
+				(parsedCity: string) => parsedCity !== city
+			);
+			localStorage.setItem(
+				'selectedCities',
+				JSON.stringify(updatedCities)
+			);
+		}
+	};
+
+	const onUpdate = async (city: string) => {
+		try {
+			await dispatch(fetchCurrentWeather(city));
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
 	return (
 		<div className="container">
 			<div className="content">
 				<div className="search">
-					<input type="text" placeholder="Search City"></input>
-					<SearchOutlinedIcon />
+					<div>
+						<input
+							type="text"
+							placeholder="Search City"
+							value={wordEntered}
+							onChange={handleFilter}></input>
+						{filteredData.length === 0 ? (
+							<SearchOutlinedIcon />
+						) : (
+							<CloseIcon onClick={clearInput} />
+						)}
+					</div>
+					{filteredData.length !== 0 && (
+						<div className="data-resulst">
+							{filteredData.slice(0, 15).map((value, key) => {
+								return (
+									<div
+										key={key}
+										className="data-item"
+										onClick={() =>
+											handleCityClick(value.name)
+										}>
+										<p>
+											{`${value.name}, ${value.country}`}
+										</p>
+									</div>
+								);
+							})}
+						</div>
+					)}
 				</div>
-				<WeatherCard weather={weather} />
+				{selectedCities.length !== 0 ? (
+					selectedCities.map((city: string, index: number) => {
+						const weatherData = weather[index];
+						return (
+							<WeatherCard
+								key={city}
+								city={city}
+								weather={weatherData}
+								onDelete={onDelete}
+								onUpdate={onUpdate}
+							/>
+						);
+					})
+				) : (
+					<Empty />
+				)}
 			</div>
 		</div>
 	);
