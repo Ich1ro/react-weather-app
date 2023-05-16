ff330eac2290536c2c8fde1bfbd93f5a3d9c96f7
ich1ro <pavel.dubenchuk.02@gmail.com>
2023-05-17 01:09:35 +0300

    use redix-persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,29 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import currentWeatherSliceReducer from './slices/currentWeatherSlice';
+import {
+	persistStore,
+	persistReducer,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers({
 	currentWeatherSliceReducer
 });
 
+const persistConfig = {
+	key: 'root',
+	storage: storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-	reducer: rootReducer,
+	reducer: persistedReducer,
 	middleware: getDefaultMiddleware =>
 		getDefaultMiddleware({ serializableCheck: false })
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppStore = typeof store;
