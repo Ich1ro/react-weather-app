9b9d2048599d69625c9ad682359f625f9483a9cd
ich1ro <pavel.dubenchuk.02@gmail.com>
2023-05-17 01:16:01 +0300

    add routes and give props for pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,28 @@ import './App.css';
 import Main from './components/Main';
 import { Route, Routes } from 'react-router';
 import CloudOutlinedIcon from '@mui/icons-material/CloudOutlined';
+import Details from './components/Details';
+import { Link } from 'react-router-dom';
+import { useCustomSelector } from './hooks/store';
 
 function App() {
+
+	const weather = useCustomSelector(
+		state => state.currentWeatherSliceReducer.weather
+	);
+
 	return (
 		<>
 			<div className="header">
-				<h3 className="title">Weather app</h3>
-				<CloudOutlinedIcon />
+				<Link to={'/'}>
+					<h3 className="title">Weather app</h3>
+					<CloudOutlinedIcon />
+				</Link>
 			</div>
 			<div className="wrapper">
 				<Routes>
-					<Route path="/" element={<Main />} />
-					<Route path="/detail" />
+					<Route path="/" element={<Main weather={weather} />} />
+					<Route path="/details/:city" element={<Details weather={weather}/>} />
 				</Routes>
 			</div>
 		</>
