b4f7ff82cb87824f4d5fe9b414615756ef182d50
ich1ro <pavel.dubenchuk.02@gmail.com>
2023-05-20 00:01:09 +0300

    add api request for weather charts

--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -1,9 +1,13 @@
 import { AxiosResponse } from 'axios';
-import { Weather } from '../store/@types/types';
+import { Weather, WeatherCharts } from '../store/@types/types';
 import api from '../axios';
 
 export class WeatherService {
 	static getCurrentWeather(city: string): Promise<AxiosResponse<Weather>> {
 		return api.get<Weather>(`/weather?q=${city}`);
-	}
+	};
+
+	static getChartsWeather(city: string): Promise<AxiosResponse<WeatherCharts>> {
+		return api.get<WeatherCharts>(`/forecast?q=${city}`);
+	};
 }
