706f7c177fe50d45350782f54e62523d05fa8c59
ich1ro <pavel.dubenchuk.02@gmail.com>
2023-05-20 00:09:01 +0300

    wrapped the application in the provider

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store, persistor } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { WeatherContextProvider } from './context/WeatherContext';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
@@ -13,9 +14,11 @@ const root = ReactDOM.createRoot(
 root.render(
 	<Provider store={store}>
 		<PersistGate loading={null} persistor={persistor}>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+			<WeatherContextProvider>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</WeatherContextProvider>
 		</PersistGate>
 	</Provider>
 );
