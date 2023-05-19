fb78cd2a0df4a5d45182b479c71ef276935f3f7e
ich1ro <pavel.dubenchuk.02@gmail.com>
2023-05-19 23:59:51 +0300

    update types

--- src/store/@types/types.ts
+++ src/store/@types/types.ts
@@ -3,29 +3,38 @@ export type Weather = {
 		temp: number;
 		feels_like: number;
 		temp_max: number;
-		temp_min:number
+		temp_min: number;
 	};
 	dt: number;
 	name: string;
-	weather: [
-		{
-			main: string;
-			icon: string;
-		}
-	];
-    sys: {
-        sunset: number
-    };
+	weather: {
+		main: string;
+		icon: string;
+	}[];
+	sys: {
+		sunset: number;
+		sunrise: number
+	};
 	wind: {
-		speed: number
-	}
+		speed: number;
+	};
+};
+
+export type WeatherCharts = {
+	list: 
+		{
+			main: {
+				temp: number;
+			};
+			weather: {
+				main: string;
+				icon: string;
+			}[];
+			dt_txt: string;
+		}[];
 };
 
 export type City = {
 	name: string;
 	country: string;
 };
-
-export type WeatherContextType = {
-	weather: Weather[]
-}
